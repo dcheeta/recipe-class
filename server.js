1257877e4031c06ff0fb2e69f8dc4a41fbaba9b4
David Tschida <dtschida@172-29-234-150.dynapool.nyu.edu>
2019-07-25 21:01:33 -0400

    added image uploading

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
 
 const recipeModel = require('./api/recipe.model');
 const recipeControllers = require('./api/recipe.controllers');
+
+require('dotenv').config();
+
 const app = express();
 
 app.use(express.static('public'));
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
+app.use(fileUpload());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json({ extended: false }));
 
-const dataBaseURL =
-  'mongodb+srv://daniel:dd2345@recipes-3k4ea.mongodb.net/test?retryWrites=true&w=majority';
+// const dataBaseURL =
+//   'mongodb+srv://daniel:dd2345@recipes-3k4ea.mongodb.net/test?retryWrites=true&w=majority';
+const dataBaseURL = process.env.DATABASE;
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/public/index.html');
@@ -26,6 +30,7 @@ app.put('/api/recipes/:id', recipeControllers.update);
 app.delete('/api/recipes/:id', recipeControllers.delete);
 app.get('/api/import', recipeControllers.import);
 app.get('/api/killall', recipeControllers.killall);
+app.post('/api/upload', recipeControllers.upload);
 
 const PORT = process.env.PORT || 3000;
 
